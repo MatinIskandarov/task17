b60eabcbf490467da510feab191c2834a7298546
Your Name <7p5n52c@code.edu.az>
2022-11-16 07:58:00 +0400

    created task

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int cem = 0;
+int long_1 = int.Parse(Console.ReadLine());
+int[] arr = new int[long_1];
+for (int i = 0; i < long_1; i++)
+{
+    arr[i] = int.Parse(Console.ReadLine());
+}
+foreach (var item in arr)
+{
+    if (item % 2 == 0)
+    {
+        cem += item;
+    }
+}
+Console.WriteLine(cem);
